4a737258add72e696563b1ede5ee0a1cd0ed58ea
yani82 <yani.iman1@gmail.com>
2020-12-22 21:08:06 -0500

    commit tree 6830a9965dd10e439f7e5f53daa6c6ef0048ceaa

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,9 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    query.present? ? self.all.select { |student| student.name.downcase.include?(query.downcase) } : self.all 
+  end 
+
 end
